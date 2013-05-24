8d52f55f418f0cbb5131069979f4441cb9193c10
heliofrota <heliofrota@gmail.com>
2013-05-24 01:19:03 -0300

    fixed #14

---
+++ java/src/test/java/org/neomatrix369/AllTests.java
@@ -0,0 +1,20 @@
+package org.neomatrix369;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import org.neomatrix369.apiworld.UtilityFunctionsTest;
+
+/**
+ * Suite class AllTests.
+ *
+ * @author helio frota http://www.heliofrota.com
+ *
+ */
+@RunWith(value = Suite.class)
+@SuiteClasses(value = {
+    UtilityFunctionsTest.class
+})
+public class AllTests {
+
+}
---
+++ java/src/test/java/org/neomatrix369/apiworld/UtilityFunctionsTest.java
@@ -0,0 +1,30 @@
+package org.neomatrix369.apiworld;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Test class UtilityFunctionsTest.
+ *
+ * @author helio frota http://www.heliofrota.com
+ *
+ */
+public class UtilityFunctionsTest {
+
+    private UtilityFunctions utilityFunctions;
+    
+    @Before
+    public void setUp() {
+        // enum singleton ?
+        //utilityFunctions = new UtilityFunctions(); 
+    }
+    
+    @Test
+    public void isAValidJSONText() {
+        // this is not a valid jsonText we need to do a refactor.
+        Assert.assertEquals(true, UtilityFunctions.isAValidJSONText("{abcde"));
+    }
+    
+    
+}
