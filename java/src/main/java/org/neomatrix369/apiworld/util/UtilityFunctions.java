80ab73431c1c102c208a0b4c3045189a9a4b9720
Raphael Ackermann <raphael.ackermann@gmail.com>
2013-09-20 20:49:40 +0200

    format

--- java/src/main/java/org/neomatrix369/apiworld/util/UtilityFunctions.java
+++ java/src/main/java/org/neomatrix369/apiworld/util/UtilityFunctions.java
@@ -38,73 +38,79 @@ import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * Util class UtilityFunctions.
  * 
  * @author Mani Sarkar
- *
+ * 
  */
 public final class UtilityFunctions {
-	
+
     private static final Logger LOGGER = LoggerFactory.getLogger(UtilityFunctions.class);
 
     /**
-     * Hide Utility Class Constructor - Utility classes should not have a
-     * public or default constructor.
+     * Hide Utility Class Constructor - Utility classes should not have a public
+     * or default constructor.
      */
     private UtilityFunctions() {
-        
+
     }
 
     /**
      * Removes the trailing separator using apache commons lang.
-     * @param urlParameterTokens String
-     * @param paramSeparator String
+     * 
+     * @param urlParameterTokens
+     *            String
+     * @param paramSeparator
+     *            String
      * @return String
      */
-    public static String dropTrailingSeparator(String urlParameterTokens,
-            String paramSeparator) {
-        return StringUtils.substringBeforeLast(urlParameterTokens, paramSeparator);
+    public static String dropTrailingSeparator(String urlParameterTokens, String paramSeparator) {
+	return StringUtils.substringBeforeLast(urlParameterTokens, paramSeparator);
     }
 
     /**
      * Checks if url has a separator using apache commons lang.
-     * @param urlString String
-     * @param commandUrlSeparator String
+     * 
+     * @param urlString
+     *            String
+     * @param commandUrlSeparator
+     *            String
      * @return boolean
      */
-    public static boolean doesHaveSeparator(String urlString,
-            String commandUrlSeparator) {
-        return StringUtils.endsWith(urlString, commandUrlSeparator);
+    public static boolean doesHaveSeparator(String urlString, String commandUrlSeparator) {
+	return StringUtils.endsWith(urlString, commandUrlSeparator);
     }
-    
 
     /**
      * Converts the token to the application/x-www-form-urlencoded MIME format.
-     * @param value String
+     * 
+     * @param value
+     *            String
      * @return String
      */
     public static String encodeToken(String token) {
-        
-        if (token == null) {
-            throw new IllegalArgumentException("The token cannot be null.");
-        }
-        
-        String encodedToken = token;
-        
-        try {
-            encodedToken = URLEncoder.encode(token, "UTF-8");
-        } catch(UnsupportedEncodingException uee) {
-            LOGGER.warn("Invalid token.");
-        }
-        
-        return encodedToken;
+
+	if (token == null) {
+	    throw new IllegalArgumentException("The token cannot be null.");
+	}
+
+	String encodedToken = token;
+
+	try {
+	    encodedToken = URLEncoder.encode(token, "UTF-8");
+	} catch (UnsupportedEncodingException uee) {
+	    LOGGER.warn("Invalid token.");
+	}
+
+	return encodedToken;
     }
 
     /**
      * .
-     * @param resultAsString String
+     * 
+     * @param resultAsString
+     *            String
      * @return boolean
      */
     public static boolean isAValidJSONText(String resultAsString) {
@@ -121,53 +127,54 @@ public final class UtilityFunctions {
 
     /**
      * .
-     * @param propertyFilename String
-     * @param propertyName String
+     * 
+     * @param propertyFilename
+     *            String
+     * @param propertyName
+     *            String
      * @return String
-     * @throws IOException exception
+     * @throws IOException
+     *             exception
      */
-    public static String readPropertyFrom(String propertyFilename,
-            String propertyName) throws IOException {
-        Properties prop = new Properties();
-        try {
-            prop.load(new FileReader(new File(propertyFilename)));
-            return prop.getProperty(propertyName);
-        } catch (FileNotFoundException e) {
-            LOGGER.info("Current path: " + new File(".").getCanonicalPath());
-            LOGGER.error(e.getMessage());
-        } catch (IOException e) {
-            LOGGER.error(e.getMessage());
-        }
-        return "";
+    public static String readPropertyFrom(String propertyFilename, String propertyName) throws IOException {
+	Properties prop = new Properties();
+	try {
+	    prop.load(new FileReader(new File(propertyFilename)));
+	    return prop.getProperty(propertyName);
+	} catch (FileNotFoundException e) {
+	    LOGGER.info("Current path: " + new File(".").getCanonicalPath());
+	    LOGGER.error(e.getMessage());
+	} catch (IOException e) {
+	    LOGGER.error(e.getMessage());
+	}
+	return "";
     }
 
-	public static Document stringToXML(String string) {
-		String localString = string.substring(1, string.length() - 1);
-		localString = localString.replaceAll(">,", ">");
-		return Jsoup.parse(localString);
-	}
-	
-	public static Boolean isAValidXML(String result) {
-		stringToXML(result);
-		return true;
+    public static Document stringToXML(String string) {
+	String localString = string.substring(1, string.length() - 1);
+	localString = localString.replaceAll(">,", ">");
+	return Jsoup.parse(localString);
+    }
+
+    public static Boolean isAValidXML(String result) {
+	stringToXML(result);
+	return true;
+    }
+
+    public static String dropStartAndEndDelimeters(String inputString, String startsWith, String endsWith) {
+	String result = inputString;
+	if (result.startsWith(startsWith)) {
+	    if (result.length() == 1) {
+		result = "";
+	    } else {
+		result = result.substring(1, result.length());
+	    }
 	}
 
-
-
-	public static String dropStartAndEndDelimeters(String inputString, String startsWith, String endsWith) {
-		String result = inputString;
-		if (result.startsWith(startsWith)) {
-			if (result.length() == 1) {
-				result = "";
-			} else {
-				result = result.substring(1, result.length());
-			}	
-		}
-		
-		if (result.endsWith(endsWith)) {
-			result = result.substring(0, result.length() - 1); 
-		}
-		
-		return result;
+	if (result.endsWith(endsWith)) {
+	    result = result.substring(0, result.length() - 1);
 	}
+
+	return result;
+    }
 }
