6c4708c62e55e169e84259f790da28f0b6f59c50
neomatrix369 <sadhak001@gmail.com>
2013-09-20 22:49:26 +0100

    Added GenericAPICommandBuilder.java

---
+++ java/src/main/java/org/neomatrix369/apiworld/GenericAPICommandBuilder.java
@@ -0,0 +1,14 @@
+package org.neomatrix369.apiworld;
+
+public class GenericAPICommandBuilder {
+
+	public GenericAPICommandBuilder(APIConnection connection, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public APIBuilder withParam(String string, String string2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
